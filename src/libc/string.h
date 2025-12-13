0f1bb96cd748cf9eec39ad6f5e60b68c06372d04
rm1300 <232829235+rm1300@users.noreply.github.com>
2025-12-13 13:16:14 +0100

    release: version boilerplate

--- src/libc/string.h
+++ src/libc/string.h
@@ -2,15 +2,18 @@
 
 #include <stddef.h>
 
-inline size_t strnlen(const char *s, size_t maxlen) {
+inline size_t strnlen(const char *s, size_t maxlen)
+{
   size_t i = 0;
-  while (i < maxlen && s[i] != '\0') {
+  while (i < maxlen && s[i] != '\0')
+  {
     i++;
   }
   return i;
 }
 
-inline size_t strlen(const char *str) {
+inline size_t strlen(const char *str)
+{
   size_t len = 0;
 
   while (str[len])
@@ -19,8 +22,10 @@ inline size_t strlen(const char *str) {
   return len;
 }
 
-inline int strcmp(const char *s1, const char *s2) {
-  while (*s1 && (*s1 == *s2)) {
+inline int strcmp(const char *s1, const char *s2)
+{
+  while (*s1 && (*s1 == *s2))
+  {
     s1++;
     s2++;
   }
@@ -28,12 +33,15 @@ inline int strcmp(const char *s1, const char *s2) {
   return *(const unsigned char *)s1 - *(const unsigned char *)s2;
 }
 
-inline char *strcat(char *dest, const char *src) {
-  while (*dest != '\0') {
+inline char *strcat(char *dest, const char *src)
+{
+  while (*dest != '\0')
+  {
     dest++;
   }
 
-  while (*src != '\0') {
+  while (*src != '\0')
+  {
     *dest = *src;
     dest++;
     src++;
@@ -44,39 +52,27 @@ inline char *strcat(char *dest, const char *src) {
   return dest;
 }
 
-inline void *memcpy(void *dest, const void *src, size_t n) {
+inline void *memcpy(void *dest, const void *src, size_t n)
+{
   char *csrc = (char *)src;
   char *cdest = (char *)dest;
 
-  for (size_t i = 0; i < n; i++) {
+  for (size_t i = 0; i < n; i++)
+  {
     cdest[i] = csrc[i];
   }
 
   return dest;
 }
 
-inline void *memset(void *s, int c, size_t len) {
+inline void *memset(void *s, int c, size_t len)
+{
   unsigned char *p = (unsigned char *)s;
 
-  while (len--) {
+  while (len--)
+  {
     *p++ = (unsigned char)c;
   }
 
   return s;
 }
-
-// inline char *strndup(const char *s, size_t n)
-// {
-//   size_t len = strnlen(s, n);
-//   char *d = malloc(len + 1);
-//   if (!d)
-//     return NULL;
-//   memcpy(d, s, len);
-//   d[len] = '\0';
-//   return d;
-// }
-
-// inline char *strdup(const char *s)
-// {
-//   return strndup(s, strlen(s));
-// }
